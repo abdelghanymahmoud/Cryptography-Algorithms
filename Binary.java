31839279dfe576e7790600acc92446b0d0f8bee2
abdelghanymahmoud <56488703+abdelghanymahmoud@users.noreply.github.com>
2019-10-12 22:30:20 +0200

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DES;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author Abdelgany
+ */
+public class Binary {
+
+    public static Vector<Integer> toBin(String str) {
+        str = str.toUpperCase();
+        Vector<Integer> res = new Vector<>();
+        for (int i = 0; i < str.length(); i++) {
+            Vector<Integer> tp = convertchar(str.charAt(i));
+            for (int j = 0; j < tp.size(); j++) {
+                res.add(tp.get(j));
+            }
+        }
+        return res;
+    }
+
+    private static Vector<Integer> convertchar(char ch) {
+        Vector<Integer> res = new Vector<>();
+        int n = value(ch);
+        while (n > 0) {
+            res.add(0, n % 2);
+            n /= 2;
+        }
+        while (res.size() < 4) {
+            res.add(0, 0);
+        }
+        return res;
+    }
+
+    private static int value(char ch) {
+        if (ch >= '0' && ch <= '9') {
+            return ch - '0';
+        }
+        return ch - 'A' + 10;
+    }
+}
